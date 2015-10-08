6e82d8c1d01451ebdb7bbb740a0f44d152f914ed
tomcornall <tom@lemonstand.com>
2015-10-07 19:14:56 -0700

    Fixes markup, removes Foundation JS.

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -1,57 +1,8 @@
 //
-// Zurb Foundation stuff
-//
-;(function ($, window, undefined) {
-  'use strict';
-
-  var $doc = $(document),
-      Modernizr = window.Modernizr;
-
-  $(document).ready(function() {
-    $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
-    $.fn.foundationButtons          ? $doc.foundationButtons() : null;
-    $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
-    $.fn.foundationNavigation       ? $doc.foundationNavigation() : null;
-    $.fn.foundationTopBar           ? $doc.foundationTopBar() : null;
-    $.fn.foundationCustomForms      ? $doc.foundationCustomForms() : null;
-    $.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
-    $.fn.foundationTabs             ? $doc.foundationTabs({callback : $.foundation.customForms.appendCustomMarkup}) : null;
-    $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
-    $.fn.foundationMagellan         ? $doc.foundationMagellan() : null;
-    $.fn.foundationClearing         ? $doc.foundationClearing() : null;
-
-    $.fn.placeholder                ? $('input, textarea').placeholder() : null;
-  });
-
-  // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
-  // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
-  // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
-  // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
-  // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
-
-  // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
-  if (Modernizr.touch && !window.location.hash) {
-    $(window).load(function () {
-      setTimeout(function () {
-        window.scrollTo(0, 1);
-      }, 0);
-    });
-  }
-
-})(jQuery, this);
-
-//
 // Theme scripts
 //
 
 (function ($) {
-  // 
-  // Automatically apply Foundation custom form styles when an AJAX request finishes
-  //
-  $(window).on('onAfterAjaxUpdate', function(){
-    $(document).foundationCustomForms();
-    $("select[data-ajax-refresh]").change() // Forces Foundation to redraw the drop-down for states select elements after update
-  });
 
   $(document).ready(function() {
     // 
@@ -98,27 +49,6 @@
         update: {'#checkout-totals': 'shop-checkout-totals', '#mini-cart':'shop-minicart'}
       })
     });
-
-    //
-    // Apply Foundation form customization to payment forms
-    //
-    function foundationCustomizePaymentForms() {
-      $('#payment_form form').addClass('custom');
-      $(document).foundationCustomForms();
-      $('#payment_form form div.custom.dropdown').css('width', '100%');
-      $('#payment_form form input[type=button], #payment_form form input[type=submit]').addClass('button');
-    }
-
-    if ($('#payment_form').length) {
-      foundationCustomizePaymentForms();
-    }
-
-    $(document).on('change', '#payment_method', function() {
-      $(this).sendRequest('shop:onUpdatePaymentMethod', {
-          update: {'#payment_form' : 'shop-paymentform'},
-          onAfterUpdate: foundationCustomizePaymentForms
-      });
-    })
 
     $(document).on('click', '#copy_billing_to_shipping', function (){
       //data-ajax-handler="shop:onCopyBillingToShipping" data-ajax-update="#checkout-page=shop-checkoutaddress"
