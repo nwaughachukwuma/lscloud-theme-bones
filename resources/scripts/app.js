246aab3cec3fc4fe176b4b3f5f42d752a7c001e2
tomcornall <tom@lemonstand.com>
2015-11-05 14:28:51 -0800

    Adds view-review modal partial.

--- resources/scripts/app.js
+++ resources/scripts/app.js
@@ -148,6 +148,13 @@
           $("body").removeClass("modal-open");
         }
       });
+      $("#modal-2").on("change", function() {
+        if ($(this).is(":checked")) {
+          $("body").addClass("modal-open");
+        } else {
+          $("body").removeClass("modal-open");
+        }
+      });
 
       $(".modal-fade-screen, .modal-close").on("click", function() {
         $(".modal-state:checked").prop("checked", false).change();
